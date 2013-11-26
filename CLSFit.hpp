67b1df4d9e2596ead3ccb90d8d0cf798f64daaa7
Andreas Brauchli <a.brauchli@elementarea.net>
2013-11-25 16:34:11 -1000

    Add weighted LSFit
    Points are weighted linearly with factors 1..n in the order of the vector

--- CLSFit.hpp
+++ CLSFit.hpp
@@ -6,7 +6,7 @@ template<int D, typename X, typename Y>
 class LSFit
 {
 public:
-    LSFit(const vector<X> &x, const vector<Y> &y) : xs(x), ys(y) {
+    LSFit(const vector<X> &x, const vector<Y> &y, bool w = false) : xs(x), ys(y), weighted(w) {
         solve_cls();
     }
 
@@ -14,11 +14,18 @@ public:
         int n = xs.size();
         Mat x(n, D+1, DataType<Y>::type);
         Mat y(ys);
+        Mat w(n, 1, DataType<Y>::type);
 
         x.col(0) = Scalar(1);
         Mat(xs).convertTo(x.col(1), DataType<Y>::type);
-        for (int d = 2; d <= D; ++d) {
+        for (int d = 2; d <= D; ++d)
             pow(x.col(1), d, x.col(d));
+
+        if (weighted) {
+            for (int i = 1; i < n; ++i) {
+                x.row(i) *= i;
+                y.row(i) *= i;
+            }
         }
         solve(x, y, coef, DECOMP_QR); // alternatively DECOMP_SVD
     }
@@ -34,6 +41,7 @@ public:
         return interpolate(x);
     }
 private:
+    bool weighted;
     const vector<X> &xs;
     const vector<Y> &ys;
     Vec<Y, D+1> coef;
